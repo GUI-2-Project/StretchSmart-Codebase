1daeb2c6ba1d1ad353cb9029db8b2c6e632aa462
Void <edward_alderman@student.uml.edu>
2024-07-27 18:23:39 +0000

    add JSDoc documentation to Components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
+
+/**
+ * Footer component to be used site-wide.
+ * 
+ * @returns {JSX.Element} A rendered footer element.
+ * @example
+ * // Render footer
+ * <Footer />
+ */
+
 const Footer = () => {
     const styles = {
         footer: {
+            background: "#d9d9d9",
             minHeight: "50px"   //TODO: revisit
         }
     }
-
 
   return (
     <footer style={styles.footer}>
