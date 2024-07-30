a7859f130c32b982f51e7f46f71a03a2561ff1ef
Void <edward_alderman@student.uml.edu>
2024-07-30 14:08:29 +0000

    add database container from deployment repo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,20 +39,24 @@ const App = () => {
     };
 
     return (
-      <Router>
-        <ApolloProvider client={client}>
-          <Header 
-            isAuthenticated={isAuthenticated}
-            onLogin={handleLogin}
-            onLogout={handleLogout}
-            user="USER"
-          />
-          <StretchOverview />
-          {showMainContent}
-          <Footer />
-        </ApolloProvider>
-      </Router>
-  )
+        <Router>
+            <ApolloProvider client={client}>
+                <Header
+                    isAuthenticated={isAuthenticated}
+                    onLogin={handleLogin}
+                    onLogout={handleLogout}
+                    user="USER"
+                />
+                <Routes>
+                    <Route path="/" element={showMainContent ? <Login onLogin={handleLogin} /> : <Navigate to="/landing" />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/landing" element={<PrivateRoute element={<LandingPage onMuscleSelect={handleMuscleSelect} />} />} />
+                    <Route path="/questionnaire" element={<PrivateRoute element={<QuestionsPage />} />} />
+                </Routes>
+                {showMainContent && <Footer />}
+            </ApolloProvider>
+        </Router>
+    );
 }
 
 export default App;
